cebbbb33f13faba7769173ff29b11f910d16ceb2
Emmanuel <164503270+Emmanuel-365@users.noreply.github.com>
2025-04-10 16:30:10 +0100

    add 'codestorm-don-de-sang.onrender.com' to ALLOWED_HOSTS

--- DonDeSang/settings.py
+++ DonDeSang/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'  # Remplacez par une cl√© s√
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://codestorm-don-de-sang.onrender.com']
+ALLOWED_HOSTS = ['codestorm-don-de-sang.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
